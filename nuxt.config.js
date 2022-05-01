7cc042084b6c2c57149fa80018d023ce800e7486
usershima <shimazun2000@gmail.com>
2022-05-01 05:04:04 +0000

    change dist to docs

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,6 +30,9 @@ export default {
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
   ],
+  generate: {
+    dir: 'docs'
+  },
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
